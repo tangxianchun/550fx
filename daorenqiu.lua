9b527e98ebb19dd608521df2f6fd9ab8003b42d3
tangxianchun <137364547+tangxianchun@users.noreply.github.com>
2023-12-01 14:05:28 +0800

    Update daorenqiu.lua

--- daorenqiu.lua
+++ daorenqiu.lua
@@ -19,6 +19,23 @@
 local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end)
 
+ a:button("filesv3",function()
+  getgenv().Mode = "AI" -- "AimOnly","Rage", "AI" 
+getgenv().ForceWin = false --100% Win Need Telekenis 
+getgenv().AutoUseSkill = true 
+getgenv().BaseVelocity = 0 --Base On The Server, IDK Where To Get it It Should Be 4-6 Or 0 (Old Server) 
+getgenv().BasePredictVelocity = 3.4 -- Should be From 3-3.5 Work Good With 3 If Old Server 
+getgenv().VisualizePath = true -- false // 
+getgenv().AutoSpamClickDetect = true -- Spam Click If Near Other Player 
+getgenv().CloseRangeAttack = true -- Just Move Forward To The Last Character 
+getgenv().AutoGetVelocity = false -- Need The Ball To Be Stopped 
+getgenv().AutoClickKeyBind = "X" -- Need The Ball To Be Stopped 
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fd07660d92cb26891e9acfab9f0c6ba4.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/autoparrybest"))()
+ end)
+
  a:button("xdevslasher",function()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/xdevslasher/Siriusisbetter/main/xyz.bb.lua",true))()
  end)
